95a28e88a712db657ef3a778af3c7542dd70fc17
i44p <jsbbdjzjsjawk@gmail.com>
2025-01-29 11:24:46 +0300

    update simple inference script

--- run_patch.py
+++ run_patch.py
@@ -4,23 +4,32 @@ import torch
 from safetensors.torch import load_model
 from PIL import Image
 
+from omegaconf import OmegaConf
+
 import data.utils
 
-from modules import DPEDGenerator
+from modules.dped import DPEDModel
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("model")
+parser.add_argument("config")
 parser.add_argument("input_image")
 parser.add_argument("output_image")
 
 args = parser.parse_args()
 
-model = DPEDGenerator()
+config = OmegaConf.load(args.config)
+
+model = DPEDModel(config, 'cpu')
 load_model(model, args.model)
 
-img = data.utils.load_image(args.input_image, min_=0).unsqueeze(0)
+@torch.no_grad
+def infer():
+    img = data.utils.load_image(args.input_image, min_=0).unsqueeze(0)
 
-out_img = model(img).squeeze()
+    out_img = model.generator(img).squeeze()
 
-data.utils.save_image(out_img, args.output_image, min_=0)
+    data.utils.save_image(out_img, args.output_image, min_=0)
+
+infer()
