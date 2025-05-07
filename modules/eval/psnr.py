11d4a58a7ddea999c2f08dfc3646fb8493cc685a
i44p <jsbbdjzjsjawk@gmail.com>
2025-05-07 08:16:36 +0300

    fix psnr (incorrect mse)

--- modules/eval/psnr.py
+++ modules/eval/psnr.py
@@ -10,6 +10,7 @@ class PSNREvaluator(Evaluator):
         model.requires_grad_(False)
 
         sse = 0
+        pixels = 0
         for batch in self.dataloader:
             model_input = model.preprocessor.encode(batch[0].to(self.device))
             target = model.preprocessor.encode(batch[1].to(self.device))
@@ -17,8 +18,9 @@ class PSNREvaluator(Evaluator):
             output = model.generator(model_input)
             
             sse += torch.sum((output - target) ** 2)
+            pixels += output.numel()
         
-        mse = sse / len(self.dataloader)
+        mse = sse / pixels
 
         psnr = 10 * torch.log10(1.0 / mse)
         
