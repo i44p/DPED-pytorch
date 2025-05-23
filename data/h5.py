b9a5db68cb9c5de06878fb8b8140d11260982bb5
i44p <jsbbdjzjsjawk@gmail.com>
2025-05-23 11:42:26 +0300

    implement h5 dataset loader

--- data/h5.py
+++ data/h5.py
@@ -1,7 +1,11 @@
 import torch
+from torch.utils.data import Dataset, DataLoader
+
 import numpy as np
 import h5py as h5
-from torch.utils.data import Dataset, DataLoader
+import scipy
+import einops
+
 from PIL import Image
 
 from pathlib import Path
@@ -28,6 +32,98 @@ class H5PatchDataset(Dataset):
             target_patch = d[f"{self.target_label}/pixels"][idx]
 
         return torch.from_numpy(input_patch.copy()), torch.from_numpy(target_patch.copy())
+    
+    def get_dataloader(self):
+        dataloader = DataLoader(
+            self,
+            batch_size = self.batch_size,
+            shuffle=True,
+            num_workers=8,
+            prefetch_factor=3,
+            pin_memory=True
+        )
+        return dataloader
+
+
+class H5Dataset(Dataset):
+    def __init__(self, path: Path,
+            batch_size,
+            patch_size = 100,
+            correlation_threshold = 0.9,
+            padding_px=10,
+            guess_limit=1000,
+            *args, **kwargs
+        ):
+        self.path = Path(path)
+        self.patch_size = patch_size
+        self.correlation_threshold = correlation_threshold
+        self.padding_px = padding_px
+        self.guess_limit = guess_limit
+        self.pad = self.patch_size // 2 + self.padding_px
+        self.batch_size = batch_size
+        
+        assert self.path.is_file()
+
+        with h5.File(self.path, 'r') as d:
+            input_dataset = d[f"input"]
+            self._len = input_dataset.shape[0]
+
+    def __len__(self):
+        return self._len
+
+    def __getitem__(self, idx):
+        with h5.File(self.path, 'r') as d:
+            input_img = d[f"input"][idx]
+            target_img = d[f"target"][idx]
+        
+        if np.all(input_img == 0):
+            print(idx)
+        
+        h, w, c = input_img.shape
+
+        corel = 0
+        attempts = 0
+        while attempts <= self.guess_limit and corel < self.correlation_threshold:
+            attempts += 1
+            
+            y_center = np.random.randint(self.pad, h - self.pad)
+            x_center = np.random.randint(self.pad, w - self.pad)
+
+            input_patch = self._crop(input_img, y_center, x_center)
+            target_patch = self._crop(target_img, y_center, x_center)
+
+            if np.any(input_patch == 0) or np.any(target_patch == 0):
+                continue
+
+            corel_statistic, _ = scipy.stats.pearsonr(
+                self._rgb2gray(input_patch.astype(float) / 255),
+                self._rgb2gray(target_patch.astype(float) / 255),
+                axis=None
+            )
+
+            if np.isnan(corel_statistic):
+                continue
+            
+            corel = corel_statistic
+        
+        if attempts >= self.guess_limit:
+            raise RuntimeError(f"Failed to find a patch for an image. Dataset index: {idx}")
+        
+        return (
+            einops.rearrange(torch.from_numpy(input_patch.copy()), "h w c -> c h w"),
+            einops.rearrange(torch.from_numpy(target_patch.copy()), "h w c -> c h w")
+        )
+    
+    def _crop(self, img, y_center, x_center):
+        return img[
+            y_center - self.patch_size // 2 : y_center + self.patch_size // 2,
+            x_center - self.patch_size // 2 : x_center + self.patch_size // 2,
+            :,
+        ]
+
+    @staticmethod
+    def _rgb2gray(rgb):
+        return np.dot(rgb, [299/1000, 587/1000, 114/1000])
     
     def get_dataloader(self):
         dataloader = DataLoader(
